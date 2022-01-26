683d85dae540e3f444bfcb20b48490486cf741bf
DingYou <dingyou@live.com>
2022-01-26 10:14:49 +0800

    use iter optimize

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,21 @@ use std::process::Command;
 
 fn main() {
     let conf = load_config();
-    let mut args: Vec<String> = env::args().collect();
-    args.remove(0);
-    let (url, index) = get_url_and_index(&args);
-    let mut url = url;
-    let replaces = conf.replaces;
-    replaces
-        .iter()
-        .for_each(|f| url = url.replace(&f.from, &f.to));
-    let old = mem::replace(&mut args[index], url.to_string());
-    println!("original url: {}", old);
-    println!("replaced url: {}", args[index]);
+    let mut args = env::args();
+    args.next();
+    let args = args.map(|mut a| -> String {
+        if a.starts_with("-") {
+            return a;
+        }
+        if a.starts_with("'http") || a.starts_with("\"http") || a.starts_with("http") {
+            println!("original url: {}", a);
+            conf.replaces
+                .iter()
+                .for_each(|r| a = a.replace(&r.from, &r.to));
+            println!("replaced url: {}", a);
+        }
+        return a;
+    });
     let out = match Command::new("curl").args(args).output() {
         Ok(out) => out,
         Err(e) => panic!("{}", e),
@@ -26,16 +30,4 @@ fn main() {
     println!("{}", output_str);
     let output_err = String::from_utf8_lossy(&out.stderr);
     println!("{}", output_err);
-}
-
-fn get_url_and_index(args: &Vec<String>) -> (String, usize) {
-    for (index, arg) in args.iter().enumerate() {
-        if arg.starts_with("-") {
-            continue;
-        }
-        if arg.starts_with("'http") || arg.starts_with("\"http") || arg.starts_with("http") {
-            return (arg.to_string(), index);
-        }
-    }
-    panic!("url not found");
 }
